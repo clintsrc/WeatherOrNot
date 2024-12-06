79a2447f7e21644db3a78dc1a7f911ebe72bcced
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-06 15:35:35 -0800

    prep the vars

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -42,32 +42,68 @@ class WeatherService {
   
   // TODO: Create fetchLocationData method
   private async fetchLocationData(query: string) {
+    try {
+      const response = await fetch(query);
 
+      //const parks = await response.json();
+
+      //const mappedParks = await this.parkDataMapping(parks.data);
+      //return mappedParks;
+    } catch {
+      console.log('Error:', err);
+      return err;
+    }
   }
-  
+
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  // Query for the city's coordinates
+  private buildGeocodeQuery(): string {
+    // passed into WeatherService from the env: baseURL, apiKey, cityName
+
+    query = `${this.baseURL}/?forecast?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+    // http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
+    // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+  }
   
   // TODO: Create buildWeatherQuery method
+  // Use the city's cooridinates to build the 5-day forecast query
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   
   // TODO: Create fetchAndDestructureLocationData method
+  // ??
   // private async fetchAndDestructureLocationData() {}
   
   // TODO: Create fetchWeatherData method
+  // ??
   // private async fetchWeatherData(coordinates: Coordinates) {}
   
   // TODO: Build parseCurrentWeather method
+  // pare the 5-day response
   // private parseCurrentWeather(response: any) {}
   
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   
   // TODO: Complete getWeatherForCity method
+  // Top level?
   // async getWeatherForCity(city: string) {}
+
 }
 
 export default new WeatherService();
+
+
+/*
+Expected Flow(?):
+Call getWeatherForCity(city) with a city name.
+Inside getWeatherForCity, call fetchLocationData(city) to get the coordinates.
+Use destructureLocationData to extract latitude and longitude.
+Build the weather query using buildWeatherQuery.
+Fetch the weather data using fetchWeatherData.
+Parse the current weather data using parseCurrentWeather.
+Build the forecast array using buildForecastArray.
+Return the parsed data to the caller.
+ */
