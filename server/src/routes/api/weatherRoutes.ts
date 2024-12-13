61a1e23c5020f7b1217b12011e0b3c0a3f13ed9d
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-12 17:02:45 -0800

    Commented the weather routes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -4,10 +4,17 @@ const router = Router();
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
-// TODO: POST Request with city name to retrieve weather data
+/*
+ * POST root
+ *
+ * Request with city name to retrieve weather data and save the city to the 
+ * search history (db/searchHistory.json)
+ * 
+ * The user can select the history item again later to refresh the current 
+ * weather again for that city, similar to a bookmark
+ * 
+ */
 router.post('/', async (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
   console.info(`${req.method} request received for weather data`);
 
   const cityName = req.body.cityName;
@@ -15,9 +22,12 @@ router.post('/', async (req: Request, res: Response) => {
   if (cityName) {
     try {
       const weatherData = await WeatherService.getWeatherForCity(cityName)
-      // Use the verified city name that OpenWeather retrieved
+      
+      // Save city to search history: use the verified city name
+      //    that OpenWeather retrieved
       console.info(`${req.method} update the history: ${weatherData[0].city}`);
       await HistoryService.addCity(weatherData[0].city);
+
       res.status(200).json(weatherData);
     } catch (error) {
       res.status(500).send('Error retrieving weather data');
@@ -28,10 +38,15 @@ router.post('/', async (req: Request, res: Response) => {
   
 });
 
-// TODO: GET search history
-//router.get('/history', async (req: Request, res: Response) => {});
+/*
+ * GET /history
+ *
+ * Read the search history (from db/searchHistory.json)
+ * 
+ */
 router.get('/history', async (req: Request, res: Response) => {
   console.info(`${req.method} request received for history`);
+
   try {
     const historyData = await HistoryService.getCities();
     res.status(200).json(historyData);
@@ -40,11 +55,19 @@ router.get('/history', async (req: Request, res: Response) => {
   }
 });
 
-// * BONUS TODO: DELETE city from search history
-//router.delete('/history/:id', async (req: Request, res: Response) => {});
+/*
+ * DELETE /history/:id
+ *
+ * Remove a city from search history
+ * 
+ * The unique ID index is passed in from the client when they click the 
+ * history item
+ * 
+ */
 router.delete('/history/:id', async (req: Request, res: Response) => {
   const cityId = req.params.id;
   console.info(`${req.method} request received for history id: ${cityId}`);
+  
   if (cityId) {
     await HistoryService.removeCity(cityId);
     res.status(200).send('City deleted');
